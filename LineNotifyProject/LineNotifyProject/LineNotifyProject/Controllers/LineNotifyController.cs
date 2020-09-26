100d6f1d8835011008dbce9767371de7a5a4817d
myong3 <39517954+myong3@users.noreply.github.com>
2020-09-26 13:54:08 +0800

    ADD Project
    ADD Project

---
+++ LineNotifyProject/LineNotifyProject/LineNotifyProject/Controllers/LineNotifyController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using LineNotifyProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace LineNotifyProject.Controllers
+{
+    [Route("api/[controller]")]
+    public class LineNotifyController : Controller
+    {
+        private readonly string _notifyUrl;
+        public LineNotifyController(IConfiguration config)
+        {
+            _notifyUrl = "https://notify-api.line.me/api/notify";
+        }
+
+        // GET: api/LineNotify/SendMessage?token=PoyChang&message=HelloWorld
+        /// <summary>傳送文字訊息</summary>
+        /// <param name="token">令牌</param>
+        /// <param name="message">訊息</param>
+        [HttpGet]
+        [Route("SendMessage")]
+        public async Task<IActionResult> SendMessage(string token, string message)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_notifyUrl);
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+
+                var form = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("message", message)
+                });
+
+                await client.PostAsync("", form);
+            }
+
+            return new EmptyResult();
+        }
+
+        // POST: api/LineNotify/SendMessage
+        /// <summary>傳送文字訊息</summary>
+        /// <param name="msg">訊息</param>
+        [HttpPost]
+        [Route("SendMessage")]
+        public async Task<IActionResult> SendMessage([FromBody] MessageModel msg)
+        {
+            var access_token = "XXREfnsKffhCaEpsSZQsfXknl1Dijg6MyiGwuGIaHWi";
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_notifyUrl);
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + access_token);
+                var name = msg.resource.createdBy.displayName;
+                var uri = msg.resource._links.web.href;
+                var message = $"{name} create a new pull request！\n {uri}";
+
+                var form = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("message", message),
+
+                });
+
+                await client.PostAsync("", form);
+            }
+
+            return new EmptyResult();
+        }
+    }
+}
---
+++ LineNotifyProject/LineNotifyProject/LineNotifyProject/Models/MessageModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LineNotifyProject.Models
+{
+    public class MessageModel
+    {
+        public string subscriptionId { get; set; }
+        public int notificationId { get; set; }
+        public string id { get; set; }
+        public string eventType { get; set; }
+        public string publisherId { get; set; }
+        public Message message { get; set; }
+        public Message detailedMessage { get; set; }
+        
+        public Resource resource { get; set; }
+        //public string resourceVersion { get; set; }
+        //public string resourceContainers { get; set; }
+        //public string createdDate { get; set; }
+    }
+
+    public class Message
+    {
+        public string text { get; set; }
+        public string html { get; set; }
+        public string markdown { get; set; }
+    }
+
+    public class Resource
+    {
+        public CreatedBy createdBy { get; set; }
+        public string creationDate { get; set; }
+        public string url { get; set; }
+        public Links _links { get; set; }
+
+    }
+
+    public class CreatedBy
+    {
+        public string displayName { get; set; }
+    }
+
+    public class Links
+    {
+        public Web web { get; set; }
+    }
+
+    public class Web
+    {
+        public string href { get; set; }
+    }
+}
